c9e6ee92c71337aac31490261bc9b54aae155a22
khecaqs <khecaqs@gmail.com>
2019-02-17 01:09:39 +0800

    1rst

---
+++ api/config/core.php
@@ -0,0 +1,14 @@
+<?php
+// show error reporting
+error_reporting(E_ALL);
+ 
+// set your default time-zone
+date_default_timezone_set('Asia/Kuala_Lumpur');
+ 
+// variables used for jwt
+$key = "AIzaSyCDTQo5n_cTXlbY01pGpBPUBxvDZowH6zo";
+$iss = "http://example.org";
+$aud = "http://example.com";
+$iat = 1356999524;
+$nbf = 1357000000;
+?>
